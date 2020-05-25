24be38f4ffb67006ce5d883ff8f799301ad80a2a
Dave Regg <drregg6@gmail.com>
2020-05-25 17:41:19 -0400

    dave-regg-blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
-import React from "react"
+import React from 'react';
+
+import Layout from '../components/layout/layout';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <p>Sup brotha?</p>
+    </Layout>
+  )
 }

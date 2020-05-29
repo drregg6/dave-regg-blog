51aa3107b2669f8126416e9d4a380d986db648b1
Dave Regg <drregg6@gmail.com>
2020-05-29 19:19:40 -0400

    Homepage content styled with cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,62 @@
 import React from 'react';
+import {
+  graphql,
+  useStaticQuery,
+  Link
+} from 'gatsby';
+import styles from './index.module.scss';
 
 import Head from '../components/meta/head';
 import Layout from '../components/layout/layout';
 
 export default function Home() {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulBlogPost(limit: 3, sort: {fields: createdAt, order: DESC}) {
+        edges {
+          node {
+            author
+            slug
+            title
+            publishedDate(formatString: "DD MMM YYYY")
+            description
+            splashImage {
+              fixed {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log(data);
   return (
     <Layout>
       <Head title='Home' />
-      <p>Sup brotha?</p>
+      <div className={styles.cards}>
+        {
+          data.allContentfulBlogPost.edges.map(({ node }) => {
+            const src = node.splashImage ? node.splashImage.fixed.src : 'https://www.placehold.it/400x300';
+            return (
+              <Link to={`/blog/${node.slug}`}>
+                <div
+                  key={node.id}
+                  className={styles.card}
+                  style={{
+                    backgroundImage: `url(${src})`
+                  }}
+                >
+                  <div>
+                    <h1>{node.title}</h1>
+                    <small>{node.author}</small>
+                  </div>
+                </div>
+              </Link>
+            )
+          })
+        }
+      </div>
     </Layout>
   )
 }

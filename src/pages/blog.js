9551eb5f082c8bd4bdbe7b9c3d45b9a5809b1753
Dave Regg <drregg6@gmail.com>
2020-06-01 17:36:32 -0400

    Started prelim Blog Page styling

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -28,24 +28,46 @@ const Blog = () => {
   return (
     <Layout>
       <Head title='Blog' />
-      <ol className={styles.posts}>
-        {
-          data.allContentfulBlogPost.edges.map(({ node }) => {
-            const { title, author, slug, publishedDate, description } = node;
-            return (
-              <li className={styles.post} key={node.id}>
-                <Link to={`/blog/${slug}`} className={styles.post} key={node.id}>
-                  <h1>{title}</h1>
-                  <small>Published on {publishedDate} by {author}</small>
-                  {
-                    description && <p>{description}</p>
-                  }
-                </Link>
-              </li>
-            )
-          })
-        }
-      </ol>
+      <div className={styles.blog}>
+        <div className={styles.categories}>
+          <h2>Search by category</h2>
+          <ul>
+            <li>
+              <Link to="/blog">Programming</Link>
+            </li>
+            <li>
+              <Link to="/blog">Meta</Link>
+            </li>
+            <li>
+              <Link to="/blog">Work</Link>
+            </li>
+            <li>
+              <Link to="/blog">Mental health</Link>
+            </li>
+            <li>
+              <Link to="/blog">Other hobbies</Link>
+            </li>
+          </ul>
+        </div>
+        <ol className={styles.posts}>
+          {
+            data.allContentfulBlogPost.edges.map(({ node }) => {
+              const { title, author, slug, publishedDate, description } = node;
+              return (
+                <li className={styles.post} key={node.id}>
+                  <Link to={`/blog/${slug}`} className={styles.post} key={node.id}>
+                    <h1>{title}</h1>
+                    <small>Published on {publishedDate} by {author}</small>
+                    {
+                      description && <p>{description}</p>
+                    }
+                  </Link>
+                </li>
+              )
+            })
+          }
+        </ol>
+      </div>
     </Layout>
   )
 }

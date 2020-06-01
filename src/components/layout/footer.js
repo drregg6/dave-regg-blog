06d697a0f9e591058159e930a623c9996a01ce1d
Dave Regg <drregg6@gmail.com>
2020-06-01 12:03:50 -0400

    Footer styles are now consistent with Homepage styles

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -18,38 +18,43 @@ const Footer = () => {
   const { author } = data.site.siteMetadata;
   return (
     <footer className={styles.footer}>
-      <h2 className={styles.header}>
-        About Me
-      </h2>
-      <h2 className={styles.header}>
-        Blog Categories
-      </h2>
-      <h2 className={styles.header}>
-        Get in Contact
-      </h2>
-      <div className={styles.content}>
-        <p className={styles.about}>
-          I am a programmer from Philadelphia. Read about my life, projects, and hobbies on my blog, or check out my portfolio on my personal website.
-        </p>
+      <div className={styles.grid}>
+        <div>
+          <h2 className={styles.header}>
+            About Me
+          </h2>
+          <p className={styles.about}>
+            I am a programmer from Philadelphia. Read about my life, projects, and hobbies on my blog, or check out my portfolio on my personal website.
+          </p>
+        </div>
+        <div>
+          <h2 className={styles.header}>
+            Get in Contact
+          </h2>
+          <ul className={`${styles.contact} ${styles.list}`}>
+            <li>Personal Website</li>
+            <li>Twitter</li>
+            <li>LinkedIn</li>
+            <li>Github</li>
+          </ul>
+        </div>
+        <div>
+          <h2 className={styles.header}>
+            Categories
+          </h2>
+          <ul className={`${styles.categories} ${styles.list}`}>
+            <li>Programming</li>
+            <li>Meta</li>
+            <li>Work</li>
+            <li>Mental Health</li>
+            <li>Other Hobbies</li>
+          </ul>
+        </div>
+        
       </div>
-      <ul className={styles.content}>
-        <li>Programming</li>
-        <li>Meta</li>
-        <li>Work</li>
-        <li>Mental Health</li>
-        <li>Other Hobbies</li>
-      </ul>
-      <ul className={styles.content}>
-        <li>Personal Website</li>
-        <li>Twitter</li>
-        <li>LinkedIn</li>
-        <li>Github</li>
-      </ul>
-      <div className={styles.copyright}>
-        <p>
+      <p className={styles.copyright}>
           Copyright &copy; {new Date().getFullYear()} <a href="http://www.daveregg.com" rel="noopener noreferrer" target="_blank">{author}</a>
         </p>
-      </div>
     </footer>
   )
 }

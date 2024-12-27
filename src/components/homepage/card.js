e6581509e05d613b5cdead0e2b4918c8258eeb4a
drregg6 <dave.r.regg@gmail.com>
2024-12-26 20:35:22 -0500

    Adjusted card CSS

--- src/components/homepage/card.js
+++ src/components/homepage/card.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Link } from 'gatsby';
-import { capitalize } from '../../helpers/strHelper';
-import { GatsbyImage } from "gatsby-plugin-image";
+import React from "react"
+import { Link } from "gatsby"
+import { capitalize } from "../../helpers/strHelper"
+import { GatsbyImage } from "gatsby-plugin-image"
 
-import * as styles from './card.module.scss';
-import * as utilStyles from '../../styles/utils.module.scss';
+import * as styles from "./card.module.scss"
+import * as utilStyles from "../../styles/utils.module.scss"
 
 const Card = ({
   id,
@@ -14,36 +14,50 @@ const Card = ({
   title,
   category,
   description,
-  image
+  image,
 }) => {
   return (
     <div className={`${styles.card} ${utilStyles.quarterWidth}`} key={id}>
       <Link to={`/blog/post/${slug}`} className={styles.image}>
-        <GatsbyImage image={image} alt={description} title={title} />
-        <div className={`${utilStyles.lightBackground} ${utilStyles.aLittleSmaller} ${styles.category}`}>
+        <GatsbyImage
+          className={`${styles.gatsbyImgWr}`}
+          imgClassName={`${styles.gatsbyImg}`}
+          image={image}
+          alt={description}
+          title={title}
+        />
+        <div
+          className={`${utilStyles.lightBackground} ${utilStyles.aLittleSmaller} ${styles.category}`}
+        >
           <Link to={`/blog/category/${category}`}>{capitalize(category)}</Link>
         </div>
       </Link>
-      <header className={`${styles.header} ${utilStyles.jcenter} ${utilStyles.mtb2}`}>
+      <header
+        className={`${styles.header} ${utilStyles.jcenter} ${utilStyles.mtb2}`}
+      >
         <Link to={`/blog/post/${slug}`}>
-          <h1 className={`${utilStyles.aLittleLarger} ${utilStyles.primaryText}`}>{title}</h1>
+          <h1
+            className={`${utilStyles.aLittleLarger} ${utilStyles.primaryText}`}
+          >
+            {title}
+          </h1>
         </Link>
-        <div className={`${styles.info} ${utilStyles.mt1}`}>
+        <div className={`${styles.info}`}>
           <p className={`${utilStyles.italic} ${utilStyles.aLittleSmaller}`}>
             {author}
           </p>
-          <small className={`${utilStyles.italic} ${utilStyles.aLittleSmaller}`}>
+          <small
+            className={`${utilStyles.italic} ${utilStyles.aLittleSmaller}`}
+          >
             {publishedDate}
           </small>
         </div>
       </header>
       <div className={styles.desc}>
-        <p>
-          {description}
-        </p>
+        <p>{description}</p>
       </div>
     </div>
   )
 }
 
-export default Card;
+export default Card
